e249832dc16718bff4ef90741d3229ae2367ad50
meenu <meenakshisivakumar08@gmail.com>
2023-05-03 12:39:07 +0530

    router refactoring

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import React from 'react';
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to='/' activeClassName="is-active" exact={true}>To Home Page</NavLink>
+        <NavLink to='/create' activeClassName="is-active">To Create Page</NavLink>
+        <NavLink to='/edit' activeClassName="is-active">To Edit Page</NavLink>
+        <NavLink to='/help' activeClassName="is-active">To Help Page</NavLink>
+    </header>
+)
+
+export default Header;
