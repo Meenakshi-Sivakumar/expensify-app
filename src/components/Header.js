ee79eae5e5b576c36d6745fbe551a7dfde5729dd
meenu <meenakshisivakumar08@gmail.com>
2023-05-03 12:59:52 +0530

    corrction needed

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
         <h1>Expensify</h1>
         <NavLink to='/' activeClassName="is-active" exact={true}>To Home Page</NavLink>
         <NavLink to='/create' activeClassName="is-active">To Create Page</NavLink>
-        <NavLink to='/edit' activeClassName="is-active">To Edit Page</NavLink>
         <NavLink to='/help' activeClassName="is-active">To Help Page</NavLink>
     </header>
 )
