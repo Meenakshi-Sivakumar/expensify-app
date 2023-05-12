30ebe997a5caac7b01f213156f130cede0d69de2
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-05-12 19:11:15 +0530

    app until semi-react works well

--- src/app.js
+++ src/app.js
@@ -12,9 +12,9 @@ import getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water bill'}));
-store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(setTextFilter('water'));
+store.dispatch(addExpense({description: 'Water bill', amount: 4500}));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 6000 }));
+store.dispatch(setTextFilter('bill'));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
